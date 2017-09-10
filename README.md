bc404bfc1c0dfe39821079d3b9aa8316e20c911d
Larry Tseng <lt222888@gmail.com>
2017-09-10 16:39:14 -0400

    Add initial code and README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# arduino-leds-project
+# arduino-leds-activity
+
+Hardware
+1. Plug in the black wire into `GND`
+2. Plug in the red wire into `+5V`
+3. Plug in the white wire into `#`
+
+
+Software
+1. Download the `arduino-leds-activity` project.
+2. Install the FastLED library to Arduino.
+
---
+++ arduino-leds-activity.ino
@@ -0,0 +1,27 @@
+/**
+ * Arduino LEDs Activity
+ * by Larry Tseng 9/10/17
+ * and Westwood Robotics
+ * 
+ * Activity
+ * 
+ */
+
+#include <FastLED.h>    // Includes the FastLED library
+
+#define DATA_PIN    6   // Configuration information for LEDs
+#define COLOR_ORDER RGB       // NEED TO UPDATE!
+#define CHIPSET     WS2812    // NEED TO UPDATE!
+#define NUM_LEDS    10        // NEED TO UPDATE!
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
